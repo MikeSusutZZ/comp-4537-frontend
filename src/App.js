8c50c6201271f092ffd27c4966b8fd571148cb83
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-03-23 12:18:53 -0700

    Added PrivateRoutes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import HomePage from './pages/HomePage'
 import { Routes, Route } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import LoginPage from './pages/LoginPage'
+import { AuthProvider } from './authentication/AuthContext'
+import PrivateRoute from './authentication/PrivateRoute'
 
 function App () {
   return (
+    <AuthProvider>
       <ChakraProvider>
         <Routes>
           <Route path="/" element={<RegistrationPage />} />
           <Route path="/register" element={<RegistrationPage />} />
 
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
+          <Route path="/home" element={<PrivateRoute><HomePage /></PrivateRoute>} />
         </Routes>
       </ChakraProvider>
+    </AuthProvider>
   )
 }
 
