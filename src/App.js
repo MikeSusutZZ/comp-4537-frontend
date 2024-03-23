ad366812a54a8933692544c4c150d0a523345afd
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-03-23 12:00:00 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
-import "./App.css";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import HomePage from "./pages/HomePage/HomePage";
-import { Routes, Route } from "react-router-dom";
+import React from 'react'
+// import LandingPage from "./pages/LandingPage";
+import RegistrationPage from './pages/RegistrationPage'
+import HomePage from './pages/HomePage'
+import { Routes, Route } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+import LoginPage from './pages/LoginPage'
 
-function App() {
+function App () {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/home" element={<HomePage />} />
-      </Routes>
-    </>
-  );
+      <ChakraProvider>
+        <Routes>
+          <Route path="/" element={<RegistrationPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </ChakraProvider>
+  )
 }
 
-export default App;
+export default App
