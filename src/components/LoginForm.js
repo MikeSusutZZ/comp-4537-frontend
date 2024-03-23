10da02ffbe9d25e8f9e4bfc79356848600a72eba
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-03-23 12:36:28 -0700

    Update login to use authentication

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Form, Formik } from 'formik'
 import { registrationSchema } from '../schemas/schemas'
 import { useNavigate } from 'react-router-dom'
 import TextInput from './TextInput'
 import { VStack, Button } from '@chakra-ui/react'
+import { AuthContext } from '../authentication/AuthContext'
 
 function LoginForm () {
+  const { login } = useContext(AuthContext)
   const navigate = useNavigate()
 
   return (
@@ -14,8 +16,18 @@ function LoginForm () {
         all values are empty -- empty initial state */
         initialValues={{ email: '', password: '' }}
         validationSchema={registrationSchema}
-        onSubmit={values => {
+
+        /* Formik will validate the form fields based on the
+        above 'registrationSchema'; if these pass validation,
+        the form is submitted. */
+        onSubmit={(values, { setSubmitting }) => {
           console.log(values)
+
+          /* When the form is submitted, the 'login' function
+          from the AuthContext is called, which sets the user's
+          authentication status to 'true'. */
+          login(values)
+          setSubmitting(false)
 
           /* When user succesfully logs in, navigate to the
           home page, where the user will make API calls. */
@@ -30,9 +42,9 @@ function LoginForm () {
 
           <Button
             type="submit"
+            isLoading={formik.isSubmitting}
             w={'100%'}
-            variant='solid'
-            disabled={formik.isSubmitting}>
+            variant='solid'>
             Login
           </Button>
         </VStack>
